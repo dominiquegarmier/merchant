83767a45db8f6027798dcaa20ae54547d68e4c3e
Dominique Garmier <42445422+DominiqueGarmier@users.noreply.github.com>
2022-07-06 23:44:13 +0200

    merging changes from dev (#11)
    * temp commit
    
    * implemented DataStore using pystore backend
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ merchant/datasources/datastore.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+from datetime import datetime
+from pathlib import Path
+
+import dask.dataframe as dd
+import pandas as pd
+import pystore
+import yfinance as yf
+
+from merchant.config import get_config
+from merchant.datasources.base import DataSource
+
+COLUMN_NAMES = ('Date', 'Open', 'High', 'Low', 'Close', 'Volume')
+PYSTORE_NAME = 'datastore'
+COLLECTIONS = ('EOD',)
+
+
+class DataStore:
+
+    _path: Path
+    _store: pystore.store
+
+    def __init__(self) -> None:
+        self._path = get_config().DATA_PATH
+        if not self._path.exists():
+            self._path.mkdir(parents=True)
+        pystore.set_path(str(self._path))
+        self._store = pystore.store(PYSTORE_NAME)
+
+    def query(
+        self,
+        col: str,
+        symbol: str,
+        start: datetime | None = None,
+        end: datetime | None = None,
+    ) -> pd.DataFrame:
+        if col not in COLLECTIONS:
+            raise ValueError(
+                f'{col} is not a valid collection, must be one of {COLLECTIONS}'
+            )
+        if end is None:
+            end = datetime.now()
+
+        collection = self._store.collection(col)
+        key = f'{symbol.upper()}'
+        if key in collection.list_items():
+            ddf = collection.item(key).data
+            cond = ddf.Date <= end
+            if start is not None:
+                cond &= ddf.Date >= start
+            return ddf[cond].compute()
+
+        raise ValueError(f'{symbol.upper()} not DataStore')
+
+    def write(self, col: str, symbol: str, data: pd.DataFrame) -> None:
+        if data.columns.to_list() != list(COLUMN_NAMES):
+            raise ValueError(f'{data} has invalid format, must be {COLUMN_NAMES}')
+        if col not in COLLECTIONS:
+            raise ValueError(
+                f'{col} is not a valid collection, must be one of {COLLECTIONS}'
+            )
+
+        collection = self._store.collection(col)
+        key = f'{symbol.upper()}'
+        if key in collection.list_items():
+            ddf = collection.item(key).data
+            concat_ddf = dd.concat([ddf, data], axis=0).drop_duplicates(subset='Date')
+            concat_ddf = concat_ddf.sort_values(by='Date').reset_index(drop=True)
+            collection.write(key, concat_ddf, overwrite=True)
+        else:
+            collection.write(key, data)
+
+
+def download_symbols(symbols: list[str]) -> None:
+    _symbols_string = ' '.join(symbols)
+    data = yf.download(_symbols_string, period='max', group_by='ticker')
+
+    ds = DataStore()
+    for symbol in symbols:
+        if len(symbols) > 1:
+            symbol_data = data[symbol]
+        else:
+            symbol_data = data
+
+        symbol_data.drop(columns=['Adj Close'], inplace=True)
+        symbol_data.reset_index(inplace=True)
+        ds.write('EOD', symbol, symbol_data)
--- merchant/plots.py
+++ merchant/plots.py
@@ -1,5 +1,19 @@
 from __future__ import annotations
 
+import plotly.graph_objects as go
+import yfinance as yf
+
 
 def plot_example() -> None:
-    raise NotImplementedError()
+    msft = yf.Ticker('MSFT')
+    data = msft.history(period='2y')
+
+    fig = go.Figure(
+        data=go.Ohlc(
+            open=data['Open'],
+            high=data['High'],
+            low=data['Low'],
+            close=data['Close'],
+        )
+    )
+    fig.show()
