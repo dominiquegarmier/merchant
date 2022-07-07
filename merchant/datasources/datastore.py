ca30081783f0e81225d542385a5d33c2fc2b57bb
Dominique Garmier <dominique@garmier.ch>
2022-07-07 11:35:51 +0200

    using parquet filters for rough partition filtering

--- merchant/datasources/datastore.py
+++ merchant/datasources/datastore.py
@@ -30,6 +30,7 @@ class DataStore:
         self,
         col: str,
         symbol: str,
+        *,
         start: datetime | None = None,
         end: datetime | None = None,
     ) -> pd.DataFrame:
@@ -43,15 +44,28 @@ class DataStore:
         collection = self._store.collection(col)
         key = f'{symbol.upper()}'
         if key in collection.list_items():
-            ddf = collection.item(key).data
+            # partition filtering
+            filters = [('Close', '<=', 1000)]
+            if start is not None:
+                filters.append(('Close', '>=', 100))
+            ddf = collection.item(key, filters=filters).data
+
+            # fine grained filtering within the partition
             cond = ddf.Date <= end
             if start is not None:
                 cond &= ddf.Date >= start
-            return ddf[cond].compute()
+            return ddf[cond].reset_index(drop=True).compute()
 
         raise ValueError(f'{symbol.upper()} not DataStore')
 
-    def write(self, col: str, symbol: str, data: pd.DataFrame) -> None:
+    def write(
+        self,
+        col: str,
+        symbol: str,
+        data: pd.DataFrame,
+        *,
+        unsafe_append: bool = False,
+    ) -> None:
         if data.columns.to_list() != list(COLUMN_NAMES):
             raise ValueError(f'{data} has invalid format, must be {COLUMN_NAMES}')
         if col not in COLLECTIONS:
@@ -62,6 +76,8 @@ class DataStore:
         collection = self._store.collection(col)
         key = f'{symbol.upper()}'
         if key in collection.list_items():
+            if not unsafe_append:
+                raise ValueError(f'{symbol.upper()} already exists')
             ddf = collection.item(key).data
             concat_ddf = dd.concat([ddf, data], axis=0).drop_duplicates(subset='Date')
             concat_ddf = concat_ddf.sort_values(by='Date').reset_index(drop=True)
@@ -84,3 +100,13 @@ def download_symbols(symbols: list[str]) -> None:
         symbol_data.drop(columns=['Adj Close'], inplace=True)
         symbol_data.reset_index(inplace=True)
         ds.write('EOD', symbol, symbol_data)
+
+
+def read_symbol() -> None:
+    ds = DataStore()
+    data = ds.query('EOD', 'AAPL', start=datetime(2020, 1, 1))
+    print(data)
+
+
+download_symbols(['AAPL'])
+read_symbol()
